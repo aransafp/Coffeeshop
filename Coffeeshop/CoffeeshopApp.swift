974ee86d7cda98f557a35c6f3d3a6ee5b57253fd
aransa prawira <aransa.prawira@gopay.co.id>
2022-11-20 20:51:00 +0700

    Initial Commit

---
+++ Coffeeshop/CoffeeshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeshopApp.swift
+//  Coffeeshop
+//
+//  Created by Aransa Prawira on 20/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
