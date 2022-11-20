c2cadfd9216575984aea75e33230df14a44b6954
aransafp <aransafitraprawira@ymail.com>
2022-11-20 21:30:52 +0700

    refactor and remove unused code

--- Coffeeshop/View/CoffeeListView.swift
+++ Coffeeshop/View/CoffeeListView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct CoffeeListView: View {
+    // MARK: PROPERTIES
+    @State private var searchText: String = ""
+    
+    private var coffeeShopResults: [Coffeeshop] {
+        let results = CoffeeshopProvider.all()
+        
+        if searchText.isEmpty {
+            return results
+        }
+        
+        let filteredCoffeeShopResults: [Coffeeshop] = results.filter {
+            result in result.name.lowercased().contains(searchText.lowercased())
+        }
+        
+        return filteredCoffeeShopResults
+    }
+    
+    // MARK: VIEWS
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(coffeeShopResults) { coffeeShop in
+                Text("\(coffeeShop.name)")
+            }
+            .navigationTitle("Coffeeshop")
+            .searchable(
+                text: $searchText,
+                placement: .navigationBarDrawer(displayMode: .always),
+                prompt: "Search coffee shop"
+            )
+        }
     }
 }
 
